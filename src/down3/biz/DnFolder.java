3807492349f3e373cee39147507057552a56d677
xfl <xfl@ncmem.com>
2017-05-22 17:28:51 +0800

    优化文件块处理逻辑 优化文件信息，下载列表新增blockPath,下载块大小与上传块大小保持一致，新增文件合并标识,新增fileOffset,blockOffset 修复子文件没有更新blockPath的问题

--- src/down3/biz/DnFolder.java
+++ src/down3/biz/DnFolder.java
@@ -1,20 +1,9 @@
 package down3.biz;
 
-import com.google.gson.Gson;
-
 import down3.model.DnFolderInf;
-import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringUtils;
-
-import up7.DBFile;
 import up7.DbHelper;
-import up7.model.FileInf;
-import up7.model.FolderInf;
-
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class DnFolder 
 {
@@ -63,50 +52,5 @@ public class DnFolder
 		DbHelper db = new DbHelper();
 		db.ExecuteNonQuery("truncate table down3_folders");
 		db.ExecuteNonQuery("truncate table down3_files");
-    }
-    
-    public static void Del(String idF,String idFD,String uid,String mac)
-    {
-        String sql = "delete from down3_folders where fd_id=? and fd_mac=? and fd_uid=?";
-        DbHelper db = new DbHelper();
-        PreparedStatement cmd = db.GetCommand(sql);
-        try
-        {
-			cmd.setInt(1, Integer.parseInt(idFD) );
-			cmd.setString(2, mac);
-			cmd.setString(3, uid);
-			db.ExecuteNonQuery(cmd);
-			
-			//删除down3_files
-			sql = "delete from down3_files where f_id=? and f_mac=? and f_uid=?";
-			cmd = db.GetCommand(sql);
-			cmd.setInt(1, Integer.parseInt(idF));
-			cmd.setString(2, mac);
-			cmd.setString(3, uid);
-			db.ExecuteNonQuery(cmd);
-        }
-        catch(SQLException e)
-        {
-        	e.printStackTrace();
-        }
-    }
-    
-    public static void Update(String fid,String uid,String mac,String percent)
-    {
-        String sql = "update down3_folders set fd_percent=? where fd_id=? and fd_uid=? and fd_mac=?";
-        DbHelper db = new DbHelper();
-        PreparedStatement cmd = db.GetCommand(sql);
-        try
-        {
-			cmd.setString(1, percent );
-			cmd.setInt(2, Integer.parseInt(fid) );
-			cmd.setInt(3, Integer.parseInt(uid) );
-			cmd.setString(4, mac );
-			db.ExecuteNonQuery(cmd);
-        }
-        catch(SQLException e)
-        {
-        	e.printStackTrace();
-        }    	
     }
 }
